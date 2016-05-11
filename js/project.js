27168f8717757a95ea257a92cfab4f3080ea5e61
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-10 20:26:56 -0700

    created project script to use projects model

---
+++ js/project.js
@@ -0,0 +1,9 @@
+var projects = [];
+
+function Project (opts) {
+  this.title = opts.title;
+  this.author = opts.author;
+  this.created = opts.created;
+  this.description = opts.description;
+  this.link = opts.link;
+}
