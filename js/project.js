1570cdaab1466ab904581c25388dcb1f607b16bc
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-10 21:41:42 -0700

    finished project.js script file

--- js/project.js
+++ js/project.js
@@ -7,3 +7,25 @@ function Project (opts) {
   this.description = opts.description;
   this.link = opts.link;
 }
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('article.template').clone();
+
+  $newProject.find('h1').text(this.title);
+  $newProject.find('span').text(this.author);
+  $newProject.find('time').text(this.created);
+  $newProject.find('section.project-description').html(this.description);
+  $newProject.find('a').attr('href', this.link);
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+};
+
+localProjects.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
