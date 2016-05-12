ce6ee56517d48a6cdb629bbff228d7c1cf07e0c3
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-12 14:18:18 -0700

    used projects handlebar template for portfolioProjects

--- js/projects/project.js
+++ js/projects/project.js
@@ -1,25 +1,13 @@
 var projects = [];
 
 function Project (opts) {
-  this.title = opts.title;
-  this.author = opts.author;
-  this.created = opts.created;
-  this.description = opts.description;
-  this.link = opts.link;
+  for (key in opts) this[key] = opts[key];
 }
 
 Project.prototype.toHtml = function () {
-  var $newProject = $('article.template').clone();
-
-  $newProject.find('h1').text(this.title);
-  $newProject.find('span').text(this.author);
-  $newProject.find('time').text(this.created);
-  $newProject.find('section.project-description').html(this.description);
-  $newProject.find('a').attr('href', this.link);
-
-  $newProject.removeClass('template');
-
-  return $newProject;
+  var template = $('#project-template').html();
+  var source = Handlebars.compile(template);
+  return source(this);
 };
 
 localProjects.forEach(function(ele) {
