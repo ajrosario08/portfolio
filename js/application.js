0d01cace406e351c95722e5cd3d0f8a23e5cd5a1
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-11 22:17:58 -0700

    made nav links act as tabs to hide and display different content sections

--- js/application.js
+++ js/application.js
@@ -6,3 +6,15 @@ $('#pageHeader i').on('click', function(){
     $('#pageHeader nav').hide();
   }
 });
+
+$('#pageHeader nav').on('click', 'li', function(){
+  var $value = $(this).attr('data-content');
+  if (!($(this).find('a').text() === 'Github')) {
+    $('.tab-content').hide();
+    $('.tab-content').each(function(){
+      if($(this).attr('id') === $value){
+        $(this).show();
+      }
+    });
+  }
+});
