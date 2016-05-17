b687d6ce17274ae8256729e0ae7c26d3604deaec
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-17 14:57:51 -0700

    added all script files into IIFE's

--- js/pageController.js
+++ js/pageController.js
@@ -1,28 +1,32 @@
-var pageViewController = {};
+(function(module){
+  var pageViewController = {};
 
-pageViewController.toggleMoblieMenu = function() {
-  $('#pageHeader i').on('click', function(){
-    if ($('#page-nav').css('left') === '-240px' ) {
-      $('#page-nav').css('left', 0);
-    } else {
-      $('#page-nav').css('left', '-240px');
-    }
+  pageViewController.toggleMoblieMenu = function() {
+    $('#pageHeader i').on('click', function(){
+      if ($('#page-nav').css('left') === '-240px' ) {
+        $('#page-nav').css('left', 0);
+      } else {
+        $('#page-nav').css('left', '-240px');
+      }
+    });
+  };
+
+  pageViewController.toggleProjectsFilter = function() {
+    $('#show-filters').on('click',function(){
+      if ($(this).text() === 'Show filter') {
+        $('#projects-filters ul').show();
+        $(this).text('Hide filter');
+      } else {
+        $('#projects-filters ul').hide();
+        $(this).text('Show filter');
+      }
+    });
+  };
+
+  $(function(){
+    pageViewController.toggleMoblieMenu();
+    pageViewController.toggleProjectsFilter();
   });
-};
 
-pageViewController.toggleProjectsFilter = function() {
-  $('#show-filters').on('click',function(){
-    if ($(this).text() === 'Show filter') {
-      $('#projects-filters ul').show();
-      $(this).text('Hide filter');
-    } else {
-      $('#projects-filters ul').hide();
-      $(this).text('Show filter');
-    }
-  });
-};
-
-$(function(){
-  pageViewController.toggleMoblieMenu();
-  pageViewController.toggleProjectsFilter();
-});
+  module.pageViewController = pageViewController;
+})(window);
--- js/projects/project.js
+++ js/projects/project.js
@@ -1,32 +1,37 @@
-function Project (opts) {
-  for (key in opts) this[key] = opts[key];
-}
+(function(module){
 
-Project.all = [];
-
-Project.prototype.toHtml = function (template) {
-  var template = Handlebars.compile((template).html());
-  return template(this);
-};
-
-Project.loadAll = function(dataWePassIn) {
-  dataWePassIn.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function() {
-  if (localStorage.portfolioProjects) {
-    Project.processData(JSON.parse(localStorage.portfolioProjects));
-  } else {
-    $.getJSON('data/portfolioProjects.json', function(data) {
-      Project.processData(data);
-    });
+  function Project (opts) {
+    for (key in opts) this[key] = opts[key];
   }
-};
 
-Project.processData = function(data) {
-  Project.loadAll(data);
-  localStorage.setItem('portfolioProjects', JSON.stringify(data));
-  projectsView.initIndexPage();
-};
+  Project.all = [];
+
+  Project.prototype.toHtml = function (template) {
+    var template = Handlebars.compile((template).html());
+    return template(this);
+  };
+
+  Project.loadAll = function(dataWePassIn) {
+    Project.all = dataWePassIn.map(function(ele){
+      return new Project(ele);
+    });
+  };
+
+  Project.fetchAll = function() {
+    if (localStorage.portfolioProjects) {
+      Project.processData(JSON.parse(localStorage.portfolioProjects));
+    } else {
+      $.getJSON('data/portfolioProjects.json', function(data) {
+        Project.processData(data);
+      });
+    }
+  };
+
+  Project.processData = function(data) {
+    Project.loadAll(data);
+    localStorage.setItem('portfolioProjects', JSON.stringify(data));
+    projectsView.initIndexPage();
+  };
+
+  module.Project = Project;
+})(window);
