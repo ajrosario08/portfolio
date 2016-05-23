71fe04c0cf3211cafa924dafddcf928f3318de51
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-23 15:05:59 -0700

    removed handleMainNav.  functionality has been pass onto routes.js

--- js/views/projectsView.js
+++ js/views/projectsView.js
@@ -21,22 +21,6 @@
     });
   };
 
-  projectsView.handleMainNav = function() {
-
-    $('#pageHeader nav').on('click', 'li', function(){
-      var $value = $(this).attr('data-content');
-      if (!($(this).find('a').text() === 'Github')) {
-        $('.tab-content').hide();
-        $('.tab-content').each(function(){
-          if($(this).attr('id') === $value){
-            $(this).show();
-          }
-        });
-      }
-    });
-
-  };
-
   projectsView.handleTruncatedDescription = function() {
     var minimizedElements = $('.project-description p');
     var minimizeCharacterCount = 100;
@@ -71,7 +55,6 @@
       $('#projects').append(a.toHtml($('#project-template')));
 
     });
-    projectsView.handleMainNav();
     projectsView.handleProjectsFilter();
     projectsView.handleTruncatedDescription();
   };
